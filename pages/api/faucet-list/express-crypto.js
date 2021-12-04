f85402a77dc0ee8b11ea6163aa7001b49ac3d110
Dharmendra <dhar.kg@gmail.com>
2021-12-04 21:56:19 +0530

    test

--- pages/api/faucet-list/express-crypto.js
+++ pages/api/faucet-list/express-crypto.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-    const { data, status } = await axios.get('https://faucetworld.in/faucet-list/data.json');
+    const { data, status } = await axios.get('https://faucetworld.in/api-data/express-crypto-list.php');
     if (status === 200) {
         res.status(200).send(data)
     } else {
